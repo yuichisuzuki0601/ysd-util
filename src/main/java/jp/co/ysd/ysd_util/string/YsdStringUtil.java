e21c7f4a167d9d3fd189efe7736241c0a0a434d6
yuichi <ys.development0601@gmail.com>
2021-07-13 04:38:20 +0900

    単語を単数形/複数形に変換する関数を追加

--- src/main/java/jp/co/ysd/ysd_util/string/YsdStringUtil.java
+++ src/main/java/jp/co/ysd/ysd_util/string/YsdStringUtil.java
@@ -14,7 +14,7 @@ public final class YsdStringUtil {
 	 * 改行、タブ、連続スペースなどを全て半角スペースに変換します
 	 *
 	 * @param org
-	 * @return
+	 * @return dest
 	 */
 	public static String strip(String org) {
 		String dest = org.replaceAll("[\\r|\\n|\\\t]", " ");
@@ -28,7 +28,7 @@ public final class YsdStringUtil {
 	 *
 	 * @param org
 	 * @param digit
-	 * @return
+	 * @return dest
 	 */
 	public static String zeroPadding(String org, int digit) {
 		return String.format("%" + digit + "s", org).replace(" ", "0");
@@ -38,11 +38,31 @@ public final class YsdStringUtil {
 	 * 先頭の連続した0を除去します
 	 *
 	 * @param org
-	 * @return
+	 * @return dest
 	 */
 	public static String zeroSuppress(Object org) {
 		String result = org.toString().replaceFirst("^0+", "");
 		return result != null && !result.equals("") ? result : "0";
+	}
+
+	/**
+	 * 単語を単数形にします
+	 *
+	 * @param org
+	 * @return dest
+	 */
+	public static String singularize(Object org) {
+		return Inflector.getInstance().singularize(org);
+	}
+
+	/**
+	 * 単語を複数形にします
+	 *
+	 * @param org
+	 * @return dest
+	 */
+	public static String pluralize(Object org) {
+		return Inflector.getInstance().pluralize(org);
 	}
 
 }
