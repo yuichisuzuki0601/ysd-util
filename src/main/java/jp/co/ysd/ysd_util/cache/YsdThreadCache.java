7182c4e6b74bf3e27689dfea44b414dfb6b05319
yuichi <ys.development0601@gmail.com>
2022-04-08 12:38:43 +0900

    スレッドセーフなCacheMapの除去

---
+++ src/main/java/jp/co/ysd/ysd_util/cache/YsdThreadCache.java
@@ -0,0 +1,56 @@
+package jp.co.ysd.ysd_util.cache;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ *
+ * @author yuichi
+ *
+ */
+public final class YsdThreadCache {
+
+	private static Map<Thread, Map<String, Object>> threadMaps = new HashMap<>();
+
+	private static void invalidateDeadThreadCache() {
+		for (Iterator<Thread> it = threadMaps.keySet().iterator(); it.hasNext();) {
+			Thread thread = it.next();
+			if (!thread.isAlive()) {
+				threadMaps.remove(thread);
+			}
+		}
+	}
+
+	public static void invalidateCurrentThreadCache() {
+		threadMaps.remove(Thread.currentThread());
+	}
+
+	public static Object get(String key) {
+		invalidateDeadThreadCache();
+		Map<String, Object> map = threadMaps.get(Thread.currentThread());
+		if (map == null) {
+			return null;
+		} else {
+			return map.get(key);
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public static <T> T getOrDefault(String key, T defaultValue) {
+		T value = (T) get(key);
+		return value != null ? value : defaultValue;
+	}
+
+	public static void put(String key, Object value) {
+		invalidateDeadThreadCache();
+		Thread thread = Thread.currentThread();
+		Map<String, Object> map = threadMaps.get(thread);
+		if (map == null) {
+			map = new HashMap<>();
+		}
+		map.put(key, value);
+		threadMaps.put(thread, map);
+	}
+
+}
